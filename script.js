239d8e77a52ffdce9ff2c544a15d2bd3b1eea57c
Sabin Thapa <112601289+Sabinkthapa@users.noreply.github.com>
2023-08-27 01:13:26 -0300

    adding code to retrive the content when user clicked in save button

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
-var today = dayjs();
+
+
+$(document). ready(function() {
+
+  var today = dayjs();
 $('#currentDay').text(today);
 
-$(function () {
+$(".saveBtn").click(function () {
+  // console.log('test clicked');
+  var clickedbutton = $(this); //stores query object representing clicked save button
+   // console.log(parentRow);
+  var parentRow = clickedbutton.closest('.time-block');  //stores closest ancester element with the class 'time-block'
+  var hourID = parentRow.attr('id');//  stores ID attributes of parent row
+  var content =parentRow.find(".description").val(); 
+  //.find method searches for element withnt the current jquery ojbect,val method retrives the content.
 
+  localStorage.setItem(hourID, content);
 
-  
 });
 
+});
 
 
 
