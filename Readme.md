11fe699d4aa6fc9fdf8dd2cc2b664fb36ed9b748
Sabin Thapa <112601289+Sabinkthapa@users.noreply.github.com>
2023-08-27 15:48:58 -0300

    added readme.md file

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Daily Schedule Planner
+    
+## This simple application demonstrates how to create a simple work day schedule planner uses dayjs to handle the time. The app allow user to save the text and saved to the localStorage of the brower. The color of green ,red , light brown shows future , current and past hours.
+
+
+- Reason for this Project: To help user to understand how could we use jquery ,bootstrap ,googlefont to build a simple application.
+
+- Problem this project solved : This simple application would provide ideas to generate and widen the skills for bigger web applications.
+
+
+## usage 
+
+Some working application shots of this schedule planner:
+![working-application-1] (./screenshot/working-app1.png)
+![working-application-2] (./screenshot/working-app2.png)
+
+
+## Deployment Link
+
+-Please check the deloyed link below:
--- script.js
+++ script.js
@@ -5,37 +5,50 @@ $(document). ready(function() {
   var today = dayjs();
 $('#currentDay').text(today);
 
-function recognizeWithColor() {
-  var currentTime =dayjs();
-  console.log("current time",currentTime);
+function recognizeTracker() {
+  var currentHour =dayjs().hour();
+  console.log("current hourt",currentHour);
+
   $(".time-block").each(function() {
   var parentRow = $(this);
-  var hourID = parentRow.attr('id');
-  var textArea = parentRow.find('.description');
-  var hourTime = dayjs(hourID, 'h A');
-  console.log(hourTime);
+  // console.log("parent",parentRow);
+  // var idAttribute = parentRow.attr('id');
+  // console.log('attribut',idAttribute);
 
-  if (hourTime.isBefore(currentTime, "hour")) {
-    parentRow.addClass('past').removeClass('present future');
-  } else if (hourTime.isAfter(currentTime , "hour")) {
-  parentRow.addClass('future').removeClass('past current');
-  } else {
-  parentRow.addClass('current').removeClass('past future');
+  var numericHour =parseInt($(this).attr("id").split("hour")[1]);
+  console.log("hour",numericHour);
+  // var hourTime = dayjs().hour(numericHour);
+  // console.log("hours",hourTime);
+
+  if (numericHour < currentHour) {
+    $(this).addClass('past');
+    $(this).removeClass('future');
+    $(this).removeClass('present ');
   }
-})
+  else if (numericHour === currentHour) {
+    $(this).addClass('present');
+    $(this).removeClass('future');
+    $(this).removeClass('past');
+  } else {
+    $(this).addClass('future');
+    $(this).removeClass('past');
+    $(this).removeClass('present');
+  }
+});
 }
+
 
 $(".saveBtn").click(function () {
   // console.log('test clicked');
   var clickedbutton = $(this); //stores query object representing clicked save button
    // console.log(parentRow);
   var parentRow = clickedbutton.closest('.time-block');  //stores closest ancester element with the class 'time-block'
-  var hourID = parentRow.attr('id');//  stores ID attributes of parent row
+  var hourID = parentRow.attr('id');
+  console.log(hourID);//  stores ID attributes of parent row
   var textArea = parentRow.find(".description");
   var content = textArea.val();
   //.find method searches for element withnt the current jquery ojbect,val method retrives the content.
   localStorage.setItem(hourID, content);
-  recognizeWithColor(); //
   });
 
   // retrive and display the content from local storage
@@ -46,8 +59,9 @@ $(".saveBtn").click(function () {
   textArea.val(content);
   });
 
-  // recognizeWithColor(); 
+  recognizeTracker(); 
 
 });
+
 
 
